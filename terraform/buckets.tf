b2ebdbdf37503ae98ac1ef0fc33caea3a40213e3
Marco de Martinis <marco.dm@live.com.ar>
2021-10-06 10:53:50 -0300

    Adding Super Linter 3

--- terraform/buckets.tf
+++ terraform/buckets.tf
@@ -1,4 +1,6 @@
 resource "aws_s3_bucket" "test1bucket" {
+  #ts:skip=AWS.S3Bucket.EncryptionandKeyManagement.HIGH.0405 Need to skip this rule
+  #ts:skip=AWS.S3Bucket.LM.MEDIUM.0078 Need to skip this rule
   bucket = var.BUCKET_NAME
   acl    = "private"
   tags   = var.TAGS
--- terraform/instances.tf
+++ terraform/instances.tf
@@ -1,4 +1,6 @@
 resource "aws_instance" "test1instance" {
+  #ts:skip=AC-AW-IS-IN-M-0144 need to skip this rule
+  #ts:skip=AWS.AI.LM.HIGH.0070 need to skip this rule
   ami           = var.AMI_USEAST1
   instance_type = var.INSTANCE_TYPE
   tags          = var.TAGS
